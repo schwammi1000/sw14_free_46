5bae67e6982e2189cf8171da5790b15eba8ddcaf
schwammi1000 <schwammi1000@63278f4c-4601-0010-afa2-05995e66e08e>
2014-05-31 12:46:43 +0000

    Framework
    git-svn-id: https://svn.tugraz.at/svn/MobileApplicationApp@8 63278f4c-4601-0010-afa2-05995e66e08e

---
+++ ThePathOfTheDragon/ThePathOfTheDragon/PODGameScene.m
@@ -0,0 +1,41 @@
+//
+//  PODMyScene.m
+//  ThePathOfTheDragon
+//
+//  Created by Markus Kammerhofer on 31.05.14.
+//  Copyright (c) 2014 Markus Kammerhofer. All rights reserved.
+//
+
+#import "PODGameScene.h"
+
+@implementation PODGameScene
+
+-(id)initWithSize:(CGSize)size
+{
+    self = [super initWithSize:size];
+    
+    if(self == nil)
+        return nil;
+    
+    //Setup your Scene
+    
+    
+    return self;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+}
+
+-(void)update:(CFTimeInterval)currentTime
+{
+
+}
+
+-(void)didMoveToView:(SKView *)view
+{
+    
+}
+
+@end
---
+++ ThePathOfTheDragon/ThePathOfTheDragon/PODViewController.h
@@ -0,0 +1,14 @@
+//
+//  PODViewController.h
+//  ThePathOfTheDragon
+//
+
+//  Copyright (c) 2014 Markus Kammerhofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface PODViewController : UIViewController
+
+@end
